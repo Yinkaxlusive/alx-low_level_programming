fdddf7000f71c41de9407da03543c3dcbe2de78a
Yinkaxlusive <97adeyinkaonatola@gmail.com>
2023-10-10 14:20:15 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,18 +4,16 @@
  * main - Entry point
  * Return: Always 0
  */
-
 int main(void)
 {       
-        _putchar('_');
-        _putchar('p');
-        _putchar('u');
-        _putchar('t');
-        _putchar('c');
-        _putchar('h');
-        _putchar('a');
-        _putchar('r');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
-
 }
