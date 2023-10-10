1cf56214b2d301e298eb7195d2da7e4cc82b7c4d
Yinkaxlusive <97adeyinkaonatola@gmail.com>
2023-10-10 16:43:12 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,17 +5,19 @@
  */
 void print_alphabet_x10(void)
 {
-    char letter;
-    int i;
+	char letter;
+	int i;
+	
+	for (i = 0; i < 10; i++)
+	{
 
-    for (i = 0; i < 10; i++)
-    {
-        letter = 'a';
-        while (letter <= 'z')
-        {
-            _putchar(letter);
-            letter++;
-        }
-        _putchar('\n');
-    }
+		letter = 'a';
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+	}
+
 }
