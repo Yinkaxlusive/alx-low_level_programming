4e6a1bf6851816dde7dcd7223fb9dcfeaecf3fc8
root <root@DESKTOP-CEDHQSN>
2023-11-06 23:05:52 +0100

    How many legs does a dog have if you call his tail a leg

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,10 +1,11 @@
 #include <stdlib.h>
 #include "dog.h"
 
-
 /**
  * free_dog - frees memory allocated for a struct dog
  * @d: struct dog to free
+ *
+ * Return: void
  */
 void free_dog(dog_t *d)
 {
