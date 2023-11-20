f244d8896cb5cadef60f82ea3082e85cf36667e9
Yinkaxlusive <97adeyinkaonatola@gmail.com>
2023-11-20 23:46:13 +0100

    Print

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,41 +1,40 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
- * free_listint_safe - frees a linked list
- * @h: pointer to the first node in the linked list
- *
- * Return: number of elements in the freed list
+ * print_listint_safe - A function that prints the elementsin a  list
+ * @head: A pointer to listint_t structure
+ * Return: The number of nodes. Exits with 98 on failure
  */
-size_t free_listint_safe(listint_t **h)
+size_t print_listint_safe(const listint_t *head)
 {
- size_t len = 0;
- int diff;
- listint_t *temp;
+	size_t count = 0;
+	size_t new_node;
+	const listint_t *current = NULL;
+	const listint_t *prev = NULL;
 
- if (!h || !*h)
- return (0);
+	current = head;
 
- while (*h)
- {
- diff = *h - (*h)->next;
- if (diff > 0)
- {
- temp = (*h)->next;
- free(*h);
- *h = temp;
- len++;
- }
- else
- {
- free(*h);
- *h = NULL;
- len++;
- break;
- }
- }
+	while (current != NULL)
+	{
+		printf("[%p] %d\n", (void *)current, current->n);
+		count++;
+		current = current->next;
+		prev = head;
+		new_node = 0;
 
- *h = NULL;
-
- return (len);
+		while (new_node < count)
+		{
+			if (current == prev)
+			{
+				printf("-> [%p] %d\n", (void *)current, current->n);
+				return (count);
+			}
+			prev = prev->next;
+			new_node++;
+		}
+		if (head == NULL)
+			exit(98);
+	}
+	return (count);
 }
-
