9be318cb607208b3167ed2952c2196ea8585c51d
Yinkaxlusive <97adeyinkaonatola@gmail.com>
2023-11-20 23:54:28 +0100

    Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,40 +1,18 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
- * print_listint_safe - A function that prints the elementsin a  list
- * @head: A pointer to listint_t structure
- * Return: The number of nodes. Exits with 98 on failure
+ * print_listint - prints all the elements of a list.
+ * @h: head of a list.
+ *
+ * Return: numbers of nodes.
  */
-size_t print_listint_safe(const listint_t *head)
+size_t print_listint(const listint_t *h)
 {
-	size_t count = 0;
-	size_t new_node;
-	const listint_t *current = NULL;
-	const listint_t *prev = NULL;
-
-	current = head;
-
-	while (current != NULL)
-	{
-		printf("[%p] %d\n", (void *)current, current->n);
-		count++;
-		current = current->next;
-		prev = head;
-		new_node = 0;
-
-		while (new_node < count)
-		{
-			if (current == prev)
-			{
-				printf("-> [%p] %d\n", (void *)current, current->n);
-				return (count);
-			}
-			prev = prev->next;
-			new_node++;
-		}
-		if (head == NULL)
-			exit(98);
-	}
-	return (count);
+size_t nofnodes = 0;
+for (; h != NULL; h = h->next)
+{
+printf("%d\n", h->n);
+nofnodes++;
+}
+return (nofnodes);
 }
